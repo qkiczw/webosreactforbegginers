4a04855b1356e1531b5061a007fd2c9c31a1397a
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-07-05 20:21:17 +0200

    Added another components to app.js

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { format } from 'path';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>Hello!!!</p>;
+        return (
+            <form className='store-selector'>
+                <h2>Please enter the store</h2>
+                <input type='text' required placeholder='store name' />
+                <button type='submit'>Visit store</button>
+            </form>
+            
+        );
     }
 }
 
