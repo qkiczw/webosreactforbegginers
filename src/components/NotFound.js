4eca846692847a4863b0486b7f6bb95e4ee1c1d6
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-08-02 18:49:51 +0200

    Added new component NotFound and first lines of BrowserRouter code.

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+    render() {
+        return (
+            <h2>NOT FOUND!!!</h2>
+        )
+    }
+}
+
+export default NotFound;
