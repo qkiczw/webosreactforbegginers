58c28b3c062312b968bc4de8cedf5a31b7e9be9c
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-08-09 20:24:20 +0200

    BOOM! Fresh start from begining (new version of the course (React 16))

--- src/index.js
+++ src/index.js
@@ -1,24 +1,2 @@
 // let's go!
-import React from 'react';
-import {render} from 'react-dom';
-import './css/style.css';
-import {BrowserRouter, Match, Miss} from 'react-router';
-
-import App from './components/App'
-import StorePicker from './components/StorePicker';
-import NotFound from './components/NotFound';
-
-const Root = () => {
-    return (
-        <BrowserRouter>
-            <div>
-                <Match exactly pattern='/' component={StorePicker}/>
-                <Match exactly pattern='/store/:storeId' component={App}/>
-                <Miss component={NotFound} />
-            </div>
-                
-        </BrowserRouter>
-    )
-}
-
-render(<Root/>, document.querySelector('#main'));
+alert('Time to start')
