dbd00345f6c5b483d8ac94f16a8cda0923098ca3
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-09-05 20:47:32 +0200

    Remove from order function added

--- src/components/App.js
+++ src/components/App.js
@@ -40,6 +40,15 @@ class App extends React.Component {
 
     }
 
+    deleteFish = ( key ) => {
+        // 1. take a copy of state
+        const fishes = { ...this.state.fishes };
+        // 2. update the state
+        fishes[key] = null;
+        // 3. update state
+        this.setState({ fishes })
+    }
+
     addFish = (fish) => {
         console.log('Adding a Fish!')
         // 1. take a copy of existing state
@@ -60,6 +69,14 @@ class App extends React.Component {
         // 3. Call setState to update our state object
         this.setState({order: order})
     };
+    removeFromOrder = (key) => {
+        // 1. take a copy of state
+        const order = { ...this.state.order }
+        // 2. remove that item from order
+        delete order[key];
+        // 3. Call setState to update our state object
+        this.setState({ order: order })
+        }
     render() {
         return (
             <div className='catch-of-the-day'>
@@ -69,10 +86,15 @@ class App extends React.Component {
                         {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)}
                     </ul>
                 </div>
-                <Order fishes={this.state.fishes} order={this.state.order}/>
+                <Order 
+                    fishes={this.state.fishes} 
+                    order={this.state.order} 
+                    removeFromOrder={this.removeFromOrder}
+                />
                 <Inventory 
                     addFish={this.addFish} 
                     updateFish={this.updateFish} 
+                    deleteFish={this.deleteFish}
                     loadSampleFishes={this.loadSampleFishes}
                     fishes={this.state.fishes}
                 />
